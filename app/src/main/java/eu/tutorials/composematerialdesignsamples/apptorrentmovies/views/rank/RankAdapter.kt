30b7d130dd94ddda3f107a482bea81d8f3fe55c6
TIEX-DEV <62436754+TIEX-DEV@users.noreply.github.com>
2022-08-06 13:29:42 -0600

    rank fragments app torrent

---
+++ app/src/main/java/eu/tutorials/composematerialdesignsamples/apptorrentmovies/views/rank/RankAdapter.kt
@@ -0,0 +1,78 @@
+package eu.tutorials.composematerialdesignsamples.apptorrentmovies.views.rank
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import eu.tutorials.composematerialdesignsamples.R
+import eu.tutorials.composematerialdesignsamples.apptorrentmovies.data.model.MoviesItem
+import eu.tutorials.composematerialdesignsamples.databinding.ExploreLayoutRvBinding
+import eu.tutorials.composematerialdesignsamples.databinding.MoviesRankRvBinding
+import eu.tutorials.composematerialdesignsamples.util.addList
+import eu.tutorials.composematerialdesignsamples.util.distinctList
+import eu.tutorials.composematerialdesignsamples.util.downloadImage
+import eu.tutorials.composematerialdesignsamples.util.listeners.AdapterListener
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class RankAdapter(private val adapterListener: AdapterListener) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val moviesList:MutableList<MoviesItem> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RankViewHolder {
+        val binding: MoviesRankRvBinding = MoviesRankRvBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RankViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is RankViewHolder -> {
+                holder.bind(moviesList[position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return moviesList.size
+    }
+
+
+    fun addList(list: List<MoviesItem>) {
+        this.moviesList.addList(list)
+        notifyDataSetChanged()
+    }
+
+    fun updateList(list: List<MoviesItem>) {
+        GlobalScope.launch { moviesList.distinctList(list) }
+        notifyItemInserted(moviesList.size)
+    }
+
+    inner class RankViewHolder(itemView: MoviesRankRvBinding) : RecyclerView.ViewHolder(itemView.root) {
+        val rankMovieSort = itemView.rankMovieSort
+        val rankMovieName = itemView.rankMovieName
+        val rankMovieRating = itemView.rankMovieRating
+        val rankMovieRatingTxt = itemView.rankMovieRatingTxt
+        val rankMovieCategory = itemView.rankMovieCategory
+        val rankMovieYear = itemView.rankMovieYear
+        val rankMovieImg = itemView.rankMovieImg
+        fun bind(item: MoviesItem) = with(itemView) {
+            startAnimation(AnimationUtils.loadAnimation(this.context, R.anim.slide_right_to_left))
+            rankMovieSort.text = if(adapterPosition > 98) "99+" else (adapterPosition+1).toString().format(2)
+            rankMovieName.text = item.titleEnglish
+            rankMovieRating.rating = (item.rating?.div(2))?.toFloat()!!
+            rankMovieRatingTxt.text = item.rating.toString()
+            rankMovieCategory.text = item.genres?.get(0)
+            rankMovieYear.text = item.year.toString()
+            rankMovieImg.apply {
+                downloadImage(item.mediumCoverImage)
+                transitionName = item.backgroundImageOriginal
+            }
+            setOnClickListener {
+                adapterListener.openMovie(item.id!!, rankMovieImg)
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/eu/tutorials/composematerialdesignsamples/apptorrentmovies/views/rank/RankFragment.kt
@@ -0,0 +1,111 @@
+package eu.tutorials.composematerialdesignsamples.apptorrentmovies.views.rank
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import eu.tutorials.composematerialdesignsamples.R
+import eu.tutorials.composematerialdesignsamples.databinding.FragmentExploreBinding
+import eu.tutorials.composematerialdesignsamples.databinding.FragmentRankBinding
+import eu.tutorials.composematerialdesignsamples.util.*
+import eu.tutorials.composematerialdesignsamples.util.listeners.AdapterListener
+import org.koin.android.viewmodel.ext.android.getViewModel
+
+class RankFragment : Fragment(),
+    AdapterListener {
+
+    private lateinit var mbindig: FragmentRankBinding
+    private val rankAdapter by lazy { RankAdapter(this) }
+    private lateinit var viewModel: RankViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mbindig = FragmentRankBinding.inflate(inflater, container, false)
+        return mbindig.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = getViewModel()
+        observeObservers()
+        initRecyclerView()
+        viewsListener()
+    }
+
+    private fun viewsListener() {
+        mbindig.rankSwipeRefresh.setOnRefreshListener {
+            viewModel.refreshData()
+        }
+    }
+
+    private fun observeObservers() {
+        viewModel.observeRankMovies().observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is Resource.Loading -> mbindig.rankSwipeRefresh.isRefreshing = true
+                is Resource.Loaded -> {
+                    rankAdapter.addList(it.data!!)
+                    mbindig.rankSwipeRefresh.isRefreshing = false
+                    //mbindig.rankNoInternet.gone()
+                    mbindig.rankRV.show()
+                }
+                is Resource.Error -> {
+                    println("========== ${it.msg}")
+                    mbindig.rankSwipeRefresh.isRefreshing = false
+                    if(rankAdapter.itemCount < 10){
+                        //mbindig.rankNoInternet.show()
+                        mbindig.rankRV.gone()
+                    }
+                }
+                is Resource.NewData -> {
+                    mbindig.rankSwipeRefresh.isRefreshing = false
+                    rankAdapter.updateList(it.data!!)
+                }
+            }
+        })
+    }
+
+    private fun initRecyclerView() {
+        mbindig.rankRV.apply {
+            adapter = rankAdapter
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(
+                    recyclerView: RecyclerView,
+                    newState: Int
+                ) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (!recyclerView.canScrollVertically(1))
+                        viewModel.loadMoreData()
+                }
+            })
+        }
+    }
+
+    override fun openMovie(movieID: Int, imageView: ImageView) {
+        val extras =
+            FragmentNavigatorExtras(imageView to resources.getString(R.string.transitionName))
+        val action =
+            RankFragmentDirections.actionRankFragmentToDetailsFragment(
+                movieID
+            )
+        findNavController().navigate(action, extras)
+        activity?.hideBottomNav()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        activity?.showBottomNav()
+    }
+
+    override fun itemClicked(pos: Int) {
+        TODO("Not yet implemented")
+    }
+}
