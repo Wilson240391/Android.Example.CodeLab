b71db088a4cfbfb6fbb3f0f697f101694ce9810c
TIEX-DEV <62436754+TIEX-DEV@users.noreply.github.com>
2022-08-18 11:43:08 -0600

    arregle detail tmdb movies

--- app/src/main/java/eu/tutorials/composematerialdesignsamples/appmoviestmdb/data/remote/MovieDataSource.kt
+++ app/src/main/java/eu/tutorials/composematerialdesignsamples/appmoviestmdb/data/remote/MovieDataSource.kt
@@ -3,12 +3,14 @@ package eu.tutorials.composematerialdesignsamples.appmoviestmdb.data.remote
 import eu.tutorials.composematerialdesignsamples.appmoviestmdb.data.model.MovieList
 import eu.tutorials.composematerialdesignsamples.appmoviestmdb.domain.ApiService
 import eu.tutorials.composematerialdesignsamples.util.tmdb.Constants
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MovieDataSource(private val webService: ApiService) {
 
-    suspend fun getUpcomingMovies(): MovieList = webService.getUpcomingMovies(Constants.API_KEY)
+    suspend fun getUpcomingMovies(): MovieList = withContext(Dispatchers.IO){webService.getUpcomingMovies(Constants.API_KEY)}
 
-    suspend fun getTopRatedMovies(): MovieList = webService.getTopRatedMovies(Constants.API_KEY)
+    suspend fun getTopRatedMovies(): MovieList = withContext(Dispatchers.IO){webService.getTopRatedMovies(Constants.API_KEY)}
 
-    suspend fun getPopularMovies(): MovieList = webService.getPopulardMovies(Constants.API_KEY)
+    suspend fun getPopularMovies(): MovieList = withContext(Dispatchers.IO){webService.getPopulardMovies(Constants.API_KEY)}
 }
--- app/src/main/java/eu/tutorials/composematerialdesignsamples/appmoviestmdb/viewmodel/MovieViewModel.kt
+++ app/src/main/java/eu/tutorials/composematerialdesignsamples/appmoviestmdb/viewmodel/MovieViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import eu.tutorials.composematerialdesignsamples.appmoviestmdb.domain.MovieRepository
 import eu.tutorials.composematerialdesignsamples.apprepositoryvideos.views.VideosViewModel
 import eu.tutorials.composematerialdesignsamples.util.tmdb.Resource
@@ -11,7 +12,7 @@ import kotlinx.coroutines.Dispatchers
 
 class MovieViewModel(private val repo: MovieRepository): ViewModel() {
 
-    fun fetchMainScreenMovies() = liveData(Dispatchers.IO) {
+    fun fetchMainScreenMovies() = liveData(viewModelScope.coroutineContext + Dispatchers.Main) {
         emit(Resource.Loading())
         try {
             emit(Resource.Success(Triple(repo.getUpcomingMovies(),repo.getTopRatedMovies(),repo.getPopularMovies())))
